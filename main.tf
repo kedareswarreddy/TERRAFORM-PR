43a58ef6f5607829cd991ad21a46ef3d81feb3fd
kedareswarreddy <153808464+kedareswarreddy@users.noreply.github.com>
2024-03-28 12:46:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "ap-south-1"  
 }
 
-resource "aws_instance" "Terraform-jenkins automation" {
+resource "aws_instance" "Terraform-jenkins-automation" {
   ami           = "ami-0a1b648e2cd533174" # ap-south-1
   instance_type = "t2.micro"
   tags = {
