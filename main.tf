402393911761c1d30c32cc9979899e0f96755419
kedareswarreddy <153808464+kedareswarreddy@users.noreply.github.com>
2024-03-28 12:55:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "Terraform-jenkins-automation" {
   ami           = "ami-0a1b648e2cd533174" # ap-south-1
   instance_type = "t2.micro"
   tags = {
-      Name = "Terraform-jenkins"
+      Name = "terr-jenkins-1"
   }
 }
